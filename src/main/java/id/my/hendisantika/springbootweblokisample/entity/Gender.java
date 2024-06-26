64302765a49293c4b2974d909b1d2d53545afb64
hendisantika <hendisantika@yahoo.co.id>
2024-06-27 06:57:49 +0700

    Add Gender Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootweblokisample/entity/Gender.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.springbootweblokisample.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-web-loki-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/27/24
+ * Time: 06:57
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Gender {
+    MALE, FEMALE
+}
