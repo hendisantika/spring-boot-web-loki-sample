9471d45bcdc8361a11ce205c790f2ee9ede15257
hendisantika <hendisantika@yahoo.co.id>
2024-06-27 06:52:41 +0700

    Create Spring Boot Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootweblokisample/SpringBootWebLokiSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootweblokisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebLokiSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebLokiSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootweblokisample/SpringBootWebLokiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootweblokisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebLokiSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
