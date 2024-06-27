c765659a8169b66cdaa062f1ae9dc87f723608cd
hendisantika <hendisantika@yahoo.co.id>
2024-06-27 07:01:25 +0700

    Add Person in PersonController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/loki/controller/PersonController.java
+++ src/main/java/id/my/hendisantika/loki/controller/PersonController.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,5 +57,15 @@ public class PersonController {
         return persons.stream().filter(it -> it.getFirstName().equals(firstName)
                         && it.getLastName().equals(lastName))
                 .toList();
+    }
+
+    @PostMapping
+    public Person add(@RequestBody Person p) {
+        p.setId((long) (persons.size() + 1));
+        LabelMarker marker = LabelMarker.of("personId", () -> String.valueOf(p.getId()));
+        LOG.info(marker, "New person successfully added");
+        persons.add(p);
+        counterService.countNewPersons();
+        return p;
     }
 }
